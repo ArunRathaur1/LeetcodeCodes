1c16bc7911ba5d56a52b5f34c13c87d29a8f5265
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2024-10-05 08:27:44 +0530

    Time: 6 ms (68.05%), Space: 8.8 MB (80.36%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    bool compare_map(const unordered_map<char,int>&m1,const unordered_map<char,int>&m2){
-       return m1==m2;
-    }
+    // bool compare_map(const unordered_map<char,int>&m1,const unordered_map<char,int>&m2){
+    //    return m1==m2;
+    // }
     bool checkInclusion(string s1, string s2) {
         int right=s1.length();
         int end=s2.length();
         if(right>end)return false;
-        unordered_map<char,int>m1,m2;
-        for(auto c:s1)m1[c]++;
-        for(int i=0;i<right;i++)m2[s2[i]]++;
-        if(compare_map(m1,m2))return true;
+        // unordered_map<char,int>m1,m2;
+        vector<int>m1(26,0),m2(26,0);
+        for(auto c:s1)m1[c-'a']++;
+        for(int i=0;i<right;i++)m2[s2[i]-'a']++;
+        if(m1==m2)return true;
         for(int i=right;i<end;i++){
-            m2[s2[i]]++;
-            m2[s2[i-right]]--;
-            if(m2[s2[i-right]]==0)
-                m2.erase(s2[i-right]);
-            if(compare_map(m1,m2))return true;
+            m2[s2[i]-'a']++;
+           
+            if(m2[s2[i-right]-'a']!=0)
+                 m2[s2[i-right]-'a']--;
+            if(m1==m2)return true;
         }
         return false;
     }
