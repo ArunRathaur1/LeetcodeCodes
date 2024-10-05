02a66e8cc7189104c4caf1c7195ca8fe18f3813c
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2024-10-05 08:24:21 +0530

    Time: 11 ms (38.85%), Space: 10.8 MB (25.36%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    bool compare_map(const map<char,int>&m1,const map<char,int>&m2){
+    bool compare_map(const unordered_map<char,int>&m1,const unordered_map<char,int>&m2){
        return m1==m2;
     }
     bool checkInclusion(string s1, string s2) {
         int right=s1.length();
         int end=s2.length();
         if(right>end)return false;
-        map<char,int>m1,m2;
+        unordered_map<char,int>m1,m2;
         for(auto c:s1)m1[c]++;
         for(int i=0;i<right;i++)m2[s2[i]]++;
         if(compare_map(m1,m2))return true;
