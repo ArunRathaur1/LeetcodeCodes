14c83576ab54e8d85fb1d211c16a12170110a24d
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2024-10-07 07:37:27 +0530

    Time: 6 ms (57.32%), Space: 11.6 MB (61.24%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
@@ -2,28 +2,13 @@ class Solution {
 public:
     int minLength(string s) {
         int length=s.length();
-        string x=s;
-        while(true){
-            int n=x.length();
-            string p="";
-            int found=0;
-            for(int i=0;i<n;i++){
-                if(x[i]=='A'&&x[i+1]=='B'||x[i]=='C'&&x[i+1]=='D'){
-                    i++;
-                    found=1;
-                }
-                else{
-                    p=p+x[i];
-                }
-            }
-            x=p;
-            if(found==1){
-                continue;
-            }
-            else{
-                break;
-            }
+        stack<char>st;
+        for(int i=0;i<length;i++){
+            if(st.empty())st.push(s[i]);
+            else if(st.top()=='C'&& s[i]=='D')st.pop();
+            else if(st.top()=='A'&&s[i]=='B')st.pop();
+            else st.push(s[i]);
         }
-        return x.length();
+        return st.size();
     }
 };
