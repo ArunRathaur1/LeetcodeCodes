570b43b61e58049aefb33eb3334312598f2e2d07
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2024-10-18 08:10:56 +0530

    Time: 1625 ms (5.12%), Space: 751.1 MB (5.1%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
-    int countallcombination(vector<int>&nums,int value,int currvalue,int index,int n){
+    int countallcombination(vector<int>&nums,int value,int currvalue,int index,int n,vector<vector<int>>&dp){
         if(index==n and currvalue==value)return 1;
         if(index>=n)return 0;
+        if(dp[currvalue][index]!=-1)return dp[currvalue][index];
         int p=currvalue|nums[index];
-        int include=countallcombination(nums,value,p,index+1,n);
-        int exclude=countallcombination(nums,value,currvalue,index+1,n);
+        int include=countallcombination(nums,value,p,index+1,n,dp);
+        int exclude=countallcombination(nums,value,currvalue,index+1,n,dp);
+        dp[currvalue][index]=include+exclude;
         return include+exclude;
     }
     int countMaxOrSubsets(vector<int>& nums) {
@@ -14,6 +16,7 @@ public:
      for(int i=0;i<n;i++){
         maxvalue=maxvalue|nums[i];
      }  
-     return countallcombination(nums,maxvalue,0,0,n); 
+     vector<vector<int>>dp(maxvalue+1,vector<int>(n+1,-1));
+     return countallcombination(nums,maxvalue,0,0,n,dp); 
     }
 };
