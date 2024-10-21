131c9c2ba2451b151c4c3327ffe8c6598899a09b
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2024-10-21 20:30:37 +0530

    Time: 327 ms (25.73%), Space: 77.3 MB (26.02%) - LeetHub

--- 1715-split-a-string-into-the-max-number-of-unique-substrings/1715-split-a-string-into-the-max-number-of-unique-substrings.cpp
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/1715-split-a-string-into-the-max-number-of-unique-substrings.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    int maxUniqueSplit(string s) {
-        set<string> st;
-        return helper(s, st, 0);
-    }
-    
-    int helper(string& s, set<string>& st, int index) {
-        if (index == s.length()) return 0;
-        
-        int maxSplit = 0;
-        for (int i = index; i < s.length(); i++) {
-            string substring = s.substr(index, i - index + 1);
-            if (st.find(substring) == st.end()) {  // Check if it's unique
-                st.insert(substring);
-                maxSplit = max(maxSplit, 1 + helper(s, st, i + 1));  // Recur for the remaining part
-                st.erase(substring);  // Backtrack
+    int maxlength(string s,set<string>&st,int index){
+        if(index==s.length())return 0 ;
+        int maxvalue=0;
+        for(int i=index;i<s.length();i++){
+            string s1=s.substr(index,i-index+1);
+            if(st.find(s1)==st.end()){
+                st.insert(s1);
+                maxvalue=max(maxvalue,1+maxlength(s,st,i+1));
+                st.erase(s1);
             }
         }
-        return maxSplit;
+        return maxvalue;
+    }
+    int maxUniqueSplit(string s) {
+        set<string>st;
+        return maxlength(s,st,0);
+        
+
     }
 };
