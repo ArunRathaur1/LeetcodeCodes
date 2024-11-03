353d3065ce169caa41099d512c7ae10982254a94
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2024-11-03 22:47:00 +0530

    Time: 8 ms (18.75%), Space: 31.7 MB (33.51%) - LeetHub

--- 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.cpp
+++ 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.cpp
@@ -7,11 +7,11 @@ public:
        vector<int>ar=nums;
        int size=nums.size();
        for(int i=0;i<size;i++){
-         for(int j=0;j<size-1;j++){
-            if(checksamebits(ar[j],ar[j+1])&& ar[j]>ar[j+1]){
-                swap(ar[j],ar[j+1]);
+            for(int j=0;j<size-1;j++){
+                if(checksamebits(ar[j],ar[j+1])&& ar[j]>ar[j+1]){
+                    swap(ar[j],ar[j+1]);
+                }
             }
-        }
        }
         for(int i=1;i<size;i++){
             if(ar[i-1]>ar[i])return false;
