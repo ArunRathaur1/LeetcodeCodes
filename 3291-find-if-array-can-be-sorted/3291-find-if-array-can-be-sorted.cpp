c15f3121ce8a864a35ed836ddb6f9b5e3b540393
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2024-11-03 22:46:23 +0530

    Time: 7 ms (19.53%), Space: 31.7 MB (33.51%) - LeetHub

--- 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.cpp
+++ 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
+    bool checksamebits(int a,int b){
+        return __builtin_popcount(a)==__builtin_popcount(b);
+    }
     bool canSortArray(vector<int>& nums) {
-       vector<pair<int,int>>ar;
+       vector<int>ar=nums;
        int size=nums.size();
        for(int i=0;i<size;i++){
-            int tem=0;
-            int x=nums[i];
-            while(x!=0){
-                int p=x&1;
-                if(p==1)tem++;
-                x=x>>1;
-            }
-            ar.push_back({nums[i],tem});
-       }
-       for(int i=0;i<size;i++){
-        for(int j=0;j<size-1;j++){
-            if(ar[j].first>ar[j+1].first&& ar[j].second==ar[j+1].second){
+         for(int j=0;j<size-1;j++){
+            if(checksamebits(ar[j],ar[j+1])&& ar[j]>ar[j+1]){
                 swap(ar[j],ar[j+1]);
             }
         }
        }
         for(int i=1;i<size;i++){
-            if(ar[i-1].first>ar[i].first)return false;
+            if(ar[i-1]>ar[i])return false;
         }
         return true;
     }
