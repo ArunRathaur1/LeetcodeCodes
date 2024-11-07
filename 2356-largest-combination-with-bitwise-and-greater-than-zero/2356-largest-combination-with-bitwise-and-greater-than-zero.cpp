ff68aa5381ab6f631dde9a423ff95715e9f6a253
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2024-11-07 10:07:10 +0530

    Time: 24 ms (77.61%), Space: 60.2 MB (32.98%) - LeetHub

--- 2356-largest-combination-with-bitwise-and-greater-than-zero/2356-largest-combination-with-bitwise-and-greater-than-zero.cpp
+++ 2356-largest-combination-with-bitwise-and-greater-than-zero/2356-largest-combination-with-bitwise-and-greater-than-zero.cpp
@@ -1,30 +1,20 @@
 class Solution {
 public:
     int largestCombination(vector<int>& ar) {
+        vector<int>bitcount(25,0);
         int size=ar.size();
-        unordered_map<int,unordered_set<int>>m;
         for(int i=0;i<size;i++){
             int x=ar[i];
-            int count=1;
+            int count=0;
             while(x!=0){
                 int p=x&1;
                 if(p==1){
-                     m[i].insert(count);
+                    bitcount[count]++;
                 }
                 count++;
                 x=x>>1;
             }
         }
-        int maxcount=INT_MIN;
-        for(int i=1;i<25;i++){
-            int count=0;
-            for(int j=0;j<size;j++){
-                if(m[j].find(i)!=m[j].end()){
-                    count++;
-                }
-            }
-            maxcount=max(count,maxcount);
-        }
-        return maxcount;
+        return *max_element(bitcount.begin(),bitcount.end());
     }
 };
