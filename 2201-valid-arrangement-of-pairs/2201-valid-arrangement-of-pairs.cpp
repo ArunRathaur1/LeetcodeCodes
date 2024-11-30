b23880d4c74b73e94b452e765212f4f44861813a
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2024-11-30 12:15:47 +0530

    Time: 1676 ms (23.33%), Space: 294.9 MB (66.44%) - LeetHub

---
+++ 2201-valid-arrangement-of-pairs/2201-valid-arrangement-of-pairs.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    void DFS(map<int,vector<int>>& adj, int start, vector<vector<int>>& ans) {
+        while (!adj[start].empty()) {
+            int next = adj[start].back();
+            adj[start].pop_back();  // Remove the edge
+            DFS(adj, next, ans);    // Recursively visit the next node
+            ans.push_back({start, next});  // Add this edge to the result
+        }
+    }
+    
+    vector<vector<int>> validArrangement(vector<vector<int>>& pairs) {
+        map<int,vector<int>> m;
+        map<int,int> in, out;
+        
+        for (int i = 0; i < pairs.size(); ++i) {
+            m[pairs[i][0]].push_back(pairs[i][1]);
+            if (in.find(pairs[i][0]) == in.end()) {
+                in[pairs[i][0]] = 0;
+            }
+            if (out.find(pairs[i][1]) == out.end()) {
+                out[pairs[i][1]] = 0;
+            }
+            in[pairs[i][1]]++;
+            out[pairs[i][0]]++;
+        }
+
+        int start = -1;
+        for (auto& [node, outDegree] : out) {
+            if (outDegree - in[node] == 1) {
+                start = node;
+                break;
+            }
+        }
+
+        if (start == -1) {
+            start = out.begin()->first;
+        }
+
+        vector<vector<int>> ans;
+        DFS(m, start, ans);
+        
+        reverse(ans.begin(), ans.end());  // Reverse the order to get the correct path
+        return ans;
+    }
+};
