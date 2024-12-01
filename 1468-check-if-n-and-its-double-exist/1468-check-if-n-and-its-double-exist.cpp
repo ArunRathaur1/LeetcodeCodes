6b15f584a10d33a0a75c0d7bd9181d34f1a2e0f2
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2024-12-01 05:43:39 +0530

    Time: 5 ms (9.14%), Space: 15 MB (10.05%) - LeetHub

---
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        map<int,int>m;
+        for(int i=0;i<arr.size();i++){
+            m[arr[i]]=i;
+        }
+        int flag=0;
+        for(int i=0;i<arr.size();i++){
+            if(m.find(arr[i]*2)!=m.end()&& m[arr[i]*2]!=i){
+                flag=1;
+                break;
+            }
+            if(arr[i]%2==0&& m.find(arr[i]/2)!=m.end() && m[arr[i]*2]!=i){
+                flag=1;
+                break;
+            }
+        }
+        if(flag==1)return true;
+        return false;
+    }
+};
