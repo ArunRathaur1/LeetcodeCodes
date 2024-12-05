45b545c0918726188ba76b731cf25b4943f2ec83
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2024-12-05 08:42:04 +0530

    Time: 9 ms (64.37%), Space: 22 MB (33.33%) - LeetHub

--- 2414-move-pieces-to-obtain-a-string/2414-move-pieces-to-obtain-a-string.cpp
+++ 2414-move-pieces-to-obtain-a-string/2414-move-pieces-to-obtain-a-string.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
     bool canChange(string start, string target) {
-        int i1 = 0, i2 = 0;
-        int n = start.length();
-
-        while (i1 < n || i2 < n) {
-            while (i1 < n && start[i1] == '_') i1++;
-            while (i2 < n && target[i2] == '_') i2++;
-
-            if (i1 == n && i2 == n) return true;
-
-
-            if (i1 == n || i2 == n) return false;
-            if (start[i1] != target[i2]) return false;
-
-            if ((start[i1] == 'L' && i1 < i2) || (start[i1] == 'R' && i1 > i2)) return false;
-
-            i1++;
-            i2++;
-        }
-        return true;
+       int i1=0 ,i2=0;
+       int n1=start.length(),n2=target.length();
+       while(i1<n1|| i2<n2){
+        while(i1<n1&&start[i1]=='_')i1++;
+        while(i2<n2&&target[i2]=='_')i2++;
+        if(i1==n1&& i2==n2)return true;
+        if(i1==n1||i2==n2)return false;
+        if(start[i1]!=target[i2])return false;
+        if((start[i1]=='R'&&i1>i2)||(start[i1]=='L'&& i1<i2))return false;
+        i1++;
+        i2++;
+       } 
+       return true;
     }
 };
