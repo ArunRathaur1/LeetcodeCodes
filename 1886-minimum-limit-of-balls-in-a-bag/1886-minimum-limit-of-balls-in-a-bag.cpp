d0b8557135501ef54b5736dba5ff20d645c08b27
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2024-12-07 08:41:32 +0530

    Time: 27 ms (88.22%), Space: 59.8 MB (15.54%) - LeetHub

--- 1886-minimum-limit-of-balls-in-a-bag/1886-minimum-limit-of-balls-in-a-bag.cpp
+++ 1886-minimum-limit-of-balls-in-a-bag/1886-minimum-limit-of-balls-in-a-bag.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool check(int mid,vector<int>nums,int op){
+    bool check(vector<int>&nums,int op,int mid){
         for(int i=0;i<nums.size();i++){
             op=op-(nums[i]-1)/mid;
             if(op<0)return false;
@@ -8,15 +8,14 @@ public:
         return true;
     }
     int minimumSize(vector<int>& nums, int op) {
-        int ans=0;
         int left=1;
-        int right = *std::max_element(nums.begin(), nums.end());
-        ans=right;
-        while(left<=right){
+        int right=*std::max_element(nums.begin(),nums.end());
+        int ans=right;
+        while(left<right){
             int mid=(left+right)/2;
-            if(check(mid,nums,op)){
+            if(check(nums,op,mid)){
+                right=mid;
                 ans=mid;
-                right=mid-1;
             }
             else{
                 left=mid+1;
