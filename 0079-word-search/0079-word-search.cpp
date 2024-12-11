5400260c68cb753957104d730c6e3d56e6f19074
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2024-12-11 11:49:02 +0530

    Time: 809 ms (37.66%), Space: 11.7 MB (48.43%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -5,13 +5,13 @@ public:
         if(i>=row||j>=col||i<0||j<0||board[i][j]=='#'||word[index]!=board[i][j])return false;
         char tem=board[i][j];
         board[i][j]='#';
-        bool down=false,up=false,left=false,right=false;
-            down=find(board,word,i+1,j,row,col,index+1,length);
-            up=find(board,word,i-1,j,row,col,index+1,length);
-            left=find(board,word,i,j-1,row,col,index+1,length);
-            right=find(board,word,i,j+1,row,col,index+1,length);
+        bool value=
+            find(board,word,i+1,j,row,col,index+1,length)||
+            find(board,word,i-1,j,row,col,index+1,length)||
+            find(board,word,i,j-1,row,col,index+1,length)||
+            find(board,word,i,j+1,row,col,index+1,length);
         board[i][j]=tem;
-        return left||right||down||up;
+      return value;
     }
     bool exist(vector<vector<char>>& board, string word) {
         int row=board.size();
