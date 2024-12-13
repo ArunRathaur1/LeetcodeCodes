3a9d7f7f74a9de92718d329e0620e28cfa47610d
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2024-12-13 15:18:19 +0530

    Time: 0 ms (100%), Space: 13 MB (54.45%) - LeetHub

--- 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
@@ -1,27 +1,36 @@
 class Solution {
 public:
-    vector<string> generateall(int n){
-        if(n==1){
-            return {"1","0"};
-        }
-        vector<string>com=generateall(n-1);
-        vector<string>comb2;
-        for(int i=0;i<com.size();i++){
-            string x=com[i]+'0';
-            com[i]=com[i]+'1';
-            comb2.push_back(x);
-        }
-        com.insert(com.end(),comb2.begin(),comb2.end());
-        return com;
-    }
+    // vector<string> generateall(int n){
+    //     if(n==1){
+    //         return {"1","0"};
+    //     }
+    //     vector<string>com=generateall(n-1);
+    //     vector<string>comb2;
+    //     for(int i=0;i<com.size();i++){
+    //         string x=com[i]+'0';
+    //         com[i]=com[i]+'1';
+    //         comb2.push_back(x);
+    //     }
+    //     com.insert(com.end(),comb2.begin(),comb2.end());
+    //     return com;
+    // }
     string findDifferentBinaryString(vector<string>& nums) {
-        int l=nums[0].length();
-        vector<string>com=generateall(l);
+        // int l=nums[0].length();
+        // vector<string>com=generateall(l);
+        // unordered_set<string>st;
+        // for(auto i: nums)st.insert(i);
+        // int size=com.size();
+        // for(int i=0;i<size;i++){
+        //     if(st.find(com[i])==st.end())return com[i];
+        // }
+        // return "";
+        int length=nums[0].length();
         unordered_set<string>st;
         for(auto i: nums)st.insert(i);
-        int size=com.size();
-        for(int i=0;i<size;i++){
-            if(st.find(com[i])==st.end())return com[i];
+        for(int i=0;i<pow(2,length);i++){
+            std::bitset<32>s1(i);//converting to bitset
+            string s=s1.to_string().substr(32-length,length);
+            if(st.find(s)==st.end())return s;
         }
         return "";
     }
