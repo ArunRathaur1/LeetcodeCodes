7e82f92760a78efd612fb4c2a6d99b3dc89ea917
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2024-12-15 14:40:43 +0530

    Time: 401 ms (41.88%), Space: 103.2 MB (33.08%) - LeetHub

--- 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.cpp
+++ 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.cpp
@@ -1,43 +1,30 @@
 class Solution {
 public:
-    bool static customsort(pair<double, pair<double, double>> a, pair<double, pair<double, double>> b) {
-        double num1 = a.second.first;
-        double den1 = a.second.second;
-        double val1=num1/den1;
-        double value1 = (num1 + 1) / (den1 + 1);
-        double num2 = b.second.first;
-        double den2 = b.second.second;
-        double val2=num2/den2;
-        double value2 = (num2 + 1) / (den2 + 1);
-        return value1-val1 < value2-val2;
+    double maxdiff(double num,double den){
+        return (num+1)/(den+1)-(num/den);
     }
-    
     double maxAverageRatio(vector<vector<int>>& classes, int t) {
-        priority_queue<pair<double, pair<double, double>>, vector<pair<double, pair<double, double>>>, decltype(&customsort)> q(customsort);
+        priority_queue<pair<double, pair<double, double>>, vector<pair<double, pair<double, double>>>> q;
         int l = classes.size();
         double value = 0;
 
-        // Push the initial classes and their ratios into the priority queue
         for (int i = 0; i < l; i++) {
             double num = classes[i][0];
             double den = classes[i][1];
-            double p = num / den;
-            q.push({p, {num, den}});
+            q.push({maxdiff(num,den), {num, den}});
         }
 
-        // Perform the t operations
         while (t--) {
             auto [ratio, numbers] = q.top();
             q.pop();
             double num = numbers.first + 1;
             double den = numbers.second + 1;
-            double p = num / den;
-            q.push({p, {num, den}});
+            q.push({maxdiff(num,den), {num, den}});
         }
 
-        // Calculate the final average ratio
-        while (!q.empty()) {
-            value += q.top().first;
+        while (!q.empty()) {    
+            auto[gain,number]=q.top();
+            value=value+number.first/number.second;
             q.pop();
         }
         return value / l;
