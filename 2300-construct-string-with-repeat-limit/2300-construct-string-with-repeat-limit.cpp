b6dabb2683aac5ab34c37229724df8f053fb9df7
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2024-12-17 10:43:22 +0530

    Time: 87 ms (10.89%), Space: 31 MB (5.39%) - LeetHub

--- 2300-construct-string-with-repeat-limit/2300-construct-string-with-repeat-limit.cpp
+++ 2300-construct-string-with-repeat-limit/2300-construct-string-with-repeat-limit.cpp
@@ -1,46 +1,34 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
     string repeatLimitedString(string s, int repeatLimit) {
-        priority_queue<pair<char, int>> pq;
-        map<char, int> freq;
-        for (char c : s) {
-            freq[c]++;
-        }
-        for (auto it : freq) {
-            pq.push({it.first, it.second});
-        }
-
-        string ans = "";
-
-        while (!pq.empty()) {
-            pair<int,int>x=pq.top();
-            char ch=x.first;
-            int count=x.second;
-            pq.pop();
-            int useCount = min(count, repeatLimit);
-            ans.append(useCount, ch);
-            if (count > repeatLimit) {
-                if (pq.empty()) {
-                    break;
-                }
-                pair<char,int> y=pq.top();
-                int nextCount=y.second;
-                int nextCh=y.first;
-                pq.pop();
-                ans += nextCh;
-                nextCount--;
-
-                if (nextCount > 0) {
-                    pq.push({nextCh, nextCount});
-                }
-
-                pq.push({ch, count - repeatLimit});
+       priority_queue<pair<char,int>,vector<pair<char,int>>>pq;
+       map<char,int>m;
+       for(auto i:s)m[i]++;
+       for(auto i:m)pq.push({i.first,i.second});
+       string ans="";
+       while(!pq.empty()){
+        pair<char,int>x=pq.top();
+        char ch=x.first;
+        int freq=x.second;
+        pq.pop();
+        int k=min(freq,repeatLimit);
+        ans.append(string(k,ch));
+        if(freq>repeatLimit){
+            if(pq.empty()){
+                break;
             }
-        }
+            pair<char,int>y=pq.top();
+            pq.pop();
+            ans.append(1,y.first);
+            y.second--;
+            if(y.second>0){
+                pq.push(y);
+            }
+            x.second=x.second-repeatLimit;
+            pq.push(x);
 
-        return ans;
+        }
+       }
+       return ans;
     }
 };
