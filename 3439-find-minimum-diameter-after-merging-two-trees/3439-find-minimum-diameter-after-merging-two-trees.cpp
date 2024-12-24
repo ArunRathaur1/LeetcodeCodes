b044762a8683a5c140d0aaddb7505924d332c53c
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2024-12-24 18:31:54 +0530

    Time: 2019 ms (5.3%), Space: 618.7 MB (5.3%) - LeetHub

--- 3439-find-minimum-diameter-after-merging-two-trees/3439-find-minimum-diameter-after-merging-two-trees.cpp
+++ 3439-find-minimum-diameter-after-merging-two-trees/3439-find-minimum-diameter-after-merging-two-trees.cpp
@@ -1,64 +1,80 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
 class Solution {
 public:
-    // Function to construct adjacency list
-    map<int, vector<int>> adjfunction(vector<vector<int>>& edges) {
-        map<int, vector<int>> m;
-        for (auto& edge : edges) {
-            m[edge[0]].push_back(edge[1]);
-            m[edge[1]].push_back(edge[0]);
+    map<int,vector<int>> adjlist(vector<vector<int>>edges){
+        map<int,vector<int>>adj;
+        int n=edges.size();
+        for(int i=0;i<n;i++){
+            adj[edges[i][0]].push_back(edges[i][1]);
+            adj[edges[i][1]].push_back(edges[i][0]);
         }
-        return m;
+        return adj;
     }
-
-    // DFS function to calculate diameter
-    int dfs(map<int, vector<int>>& adj, int currnode, vector<bool>& visited, int& diameter) {
-        visited[currnode] = true;
-        int t1 = 0, t2 = 0;
-
-        for (int neighbor : adj[currnode]) {
-            if (!visited[neighbor]) {
-                int depth = dfs(adj, neighbor, visited, diameter);
-                if (depth > t1) {
-                    t2 = t1;
-                    t1 = depth;
-                } else if (depth > t2) {
-                    t2 = depth;
+    int findmaxnode(map<int,vector<int>>adj){
+        queue<int>q;
+        vector<bool>visited(1e5,false);
+        if (adj.empty()) return 0;
+        int start = adj.begin()->first;
+        q.push(start);
+        visited[start]=true;
+        int node=0;
+        while (!q.empty())
+        {   
+           int t=q.front();
+           visited[t]=true;
+           q.pop();
+           for(auto i: adj[t]){
+                if(visited[i]==false){
+                    q.push(i);
+                    visited[i]=true;
+                    node=i;
+                }
+           } 
+        }
+        return node;
+        
+    }
+    int finddiameter(map<int,vector<int>>adj){
+        int distance=0;
+        int node=findmaxnode(adj);
+        vector<bool>visited(1e5,false);
+        map<int,int>dis;
+        dis[node]=0;
+        queue<int>q;
+        q.push(node);
+        while(!q.empty()){
+            int t=q.front();
+            q.pop();
+            visited[t]=true;
+            for(auto i: adj[t]){
+                if(visited[i]==false){
+                    dis[i]=dis[t]+1;
+                    q.push(i);
                 }
             }
         }
-
-        diameter = max(diameter, t1 + t2);
-        return t1 + 1;
+        int maxdis=0;
+        for(auto i: dis){
+            if(maxdis<i.second){
+                maxdis=i.second;
+            }
+        }
+        return maxdis;
     }
-
     int minimumDiameterAfterMerge(vector<vector<int>>& edges1, vector<vector<int>>& edges2) {
-        auto m1 = adjfunction(edges1);
-        auto m2 = adjfunction(edges2);
-
-        int diameter1 = 0, diameter2 = 0;
-
-        if (!m1.empty()) {
-            vector<bool> visited1(1e5+1, false); // Assuming nodes are between 0 and 1000
-            dfs(m1, m1.begin()->first, visited1, diameter1);
-        }
-
-        if (!m2.empty()) {
-            vector<bool> visited2(1e5+1, false); // Assuming nodes are between 0 and 1000
-            dfs(m2, m2.begin()->first, visited2, diameter2);
-        }
-
+        map<int,vector<int>>adj1=adjlist(edges1);
+        map<int,vector<int>>adj2=adjlist(edges2);
+        int d1=finddiameter(adj1);
+        int d2=finddiameter(adj2);
         int merged_diameter = 0;
-        if (diameter1 % 2 == 0 && diameter2 % 2 == 0) {
-            merged_diameter = diameter1 / 2 + diameter2 / 2 + 1;
-        } else if (diameter1 % 2 == 0 || diameter2 % 2 == 0) {
-            merged_diameter = diameter1 / 2 + diameter2 / 2 + 2;
+        if (d1 % 2 == 0 && d2 % 2 == 0) {
+            merged_diameter = d1 / 2 + d2 / 2 + 1;
+        } else if (d1 % 2 == 0 || d2 % 2 == 0) {
+            merged_diameter = d1 / 2 + d2 / 2 + 2;
         } else {
-            merged_diameter = diameter1 / 2 + diameter2 / 2 + 3;
+            merged_diameter = d1 / 2 + d2 / 2 + 3;
         }
-
-        return max({merged_diameter, diameter1, diameter2});
+        return max({d1,d2,merged_diameter});
     }
 };
