dcf8b36d5542ddd0a7beac10e9f8cd78546a4d7f
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2024-12-24 18:32:49 +0530

    Time: 2152 ms (5.3%), Space: 488.5 MB (5.3%) - LeetHub

--- 3439-find-minimum-diameter-after-merging-two-trees/3439-find-minimum-diameter-after-merging-two-trees.cpp
+++ 3439-find-minimum-diameter-after-merging-two-trees/3439-find-minimum-diameter-after-merging-two-trees.cpp
@@ -1,72 +1,85 @@
 #include<bits/stdc++.h>
 using namespace std;
+
 class Solution {
 public:
-    map<int,vector<int>> adjlist(vector<vector<int>>edges){
-        map<int,vector<int>>adj;
-        int n=edges.size();
-        for(int i=0;i<n;i++){
+    // Converts edge list to adjacency list
+    map<int, vector<int>> adjlist(vector<vector<int>>& edges) {
+        map<int, vector<int>> adj;
+        int n = edges.size();
+        for (int i = 0; i < n; i++) {
             adj[edges[i][0]].push_back(edges[i][1]);
             adj[edges[i][1]].push_back(edges[i][0]);
         }
         return adj;
     }
-    int findmaxnode(map<int,vector<int>>adj){
-        queue<int>q;
-        vector<bool>visited(1e5,false);
+
+    // Function to find the farthest node from a given node using BFS
+    int findmaxnode(map<int, vector<int>>& adj) {
         if (adj.empty()) return 0;
-        int start = adj.begin()->first;
+
+        queue<int> q;
+        map<int, bool> visited;
+        int start = adj.begin()->first;  // Start from any node in the graph
         q.push(start);
-        visited[start]=true;
-        int node=0;
-        while (!q.empty())
-        {   
-           int t=q.front();
-           visited[t]=true;
-           q.pop();
-           for(auto i: adj[t]){
-                if(visited[i]==false){
+        visited[start] = true;
+
+        int node = start;
+        while (!q.empty()) {
+            int t = q.front();
+            q.pop();
+            for (auto i : adj[t]) {
+                if (!visited[i]) {
+                    visited[i] = true;
                     q.push(i);
-                    visited[i]=true;
-                    node=i;
+                    node = i;  // Update the farthest node
                 }
-           } 
+            }
         }
         return node;
-        
     }
-    int finddiameter(map<int,vector<int>>adj){
-        int distance=0;
-        int node=findmaxnode(adj);
-        vector<bool>visited(1e5,false);
-        map<int,int>dis;
-        dis[node]=0;
-        queue<int>q;
+
+    // Function to find the diameter of the graph (longest path between two nodes)
+    int finddiameter(map<int, vector<int>>& adj) {
+        if (adj.empty()) return 0;
+
+        // Step 1: Find the farthest node from any starting node
+        int node = findmaxnode(adj);
+
+        // Step 2: Perform BFS from the farthest node found to determine the diameter
+        map<int, bool> visited;
+        map<int, int> dis;  // Map to store distance from starting node
+        queue<int> q;
         q.push(node);
-        while(!q.empty()){
-            int t=q.front();
+        visited[node] = true;
+        dis[node] = 0;
+
+        int maxDis = 0;
+        while (!q.empty()) {
+            int t = q.front();
             q.pop();
-            visited[t]=true;
-            for(auto i: adj[t]){
-                if(visited[i]==false){
-                    dis[i]=dis[t]+1;
+            for (auto i : adj[t]) {
+                if (!visited[i]) {
+                    visited[i] = true;
+                    dis[i] = dis[t] + 1;
                     q.push(i);
+                    maxDis = max(maxDis, dis[i]);
                 }
             }
         }
-        int maxdis=0;
-        for(auto i: dis){
-            if(maxdis<i.second){
-                maxdis=i.second;
-            }
-        }
-        return maxdis;
+        return maxDis;
     }
+
+    // Function to compute the minimum diameter after merging two graphs
     int minimumDiameterAfterMerge(vector<vector<int>>& edges1, vector<vector<int>>& edges2) {
-        map<int,vector<int>>adj1=adjlist(edges1);
-        map<int,vector<int>>adj2=adjlist(edges2);
-        int d1=finddiameter(adj1);
-        int d2=finddiameter(adj2);
+        map<int, vector<int>> adj1 = adjlist(edges1);
+        map<int, vector<int>> adj2 = adjlist(edges2);
+
+        // Find the diameters of the two graphs
+        int d1 = finddiameter(adj1);
+        int d2 = finddiameter(adj2);
+
+        // Calculate merged diameter
         int merged_diameter = 0;
         if (d1 % 2 == 0 && d2 % 2 == 0) {
             merged_diameter = d1 / 2 + d2 / 2 + 1;
@@ -75,6 +88,7 @@ public:
         } else {
             merged_diameter = d1 / 2 + d2 / 2 + 3;
         }
-        return max({d1,d2,merged_diameter});
+
+        return max({d1, d2, merged_diameter});
     }
 };
