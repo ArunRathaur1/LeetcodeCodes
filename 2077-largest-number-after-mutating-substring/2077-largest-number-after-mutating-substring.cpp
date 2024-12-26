59e235f9fbae0c5503c67ffb9386e45013a21e71
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2024-12-26 11:28:15 +0530

    Time: 43 ms (5.44%), Space: 34.9 MB (12.93%) - LeetHub

--- 2077-largest-number-after-mutating-substring/2077-largest-number-after-mutating-substring.cpp
+++ 2077-largest-number-after-mutating-substring/2077-largest-number-after-mutating-substring.cpp
@@ -3,22 +3,18 @@ public:
     string maximumNumber(string num, vector<int>& change) {
         int s=num.length();
         int tem=-1;
-       for(int i=0;i<s;i++){
+        int start=0;
+        for(int i=0;i<s;i++){
         string p=to_string(num[i]);
         int q=stoi(p)-'0';
             if(q<change[q]){
-            tem=i;
-            break;
-            }
-       } 
-       if(tem==-1)return num;
-       for(int i=tem;i<s;i++){
-        string p=to_string(num[i]);
-        int q=stoi(p)-'0';
-            if(q<=change[q]){
+                if(start==0){
+                    start=i+1;
+                }
                 num[i]=char(change[q])+'0';
             }
-            else{
+            else if(q==change[q])continue;
+            else if(start!=0){
                 break;
             }
        } 
