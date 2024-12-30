9d0f49c66bace175a2c44ed4f0f50d19ff7b09fd
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2024-12-30 19:13:59 +0530

    Time: 1960 ms (5.07%), Space: 297.4 MB (8.76%) - LeetHub

--- 3348-minimum-cost-walk-in-weighted-graph/3348-minimum-cost-walk-in-weighted-graph.cpp
+++ 3348-minimum-cost-walk-in-weighted-graph/3348-minimum-cost-walk-in-weighted-graph.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-    void dfs(int node,map<int,vector<pair<int,int>>>&m,int &com_value,vector<int>&com,map<int,set<int>>&p){
+    void dfs(int node,map<int,vector<pair<int,int>>>&m,int &com_value,vector<int>&com,vector<int>&p){
         if(m[node].size()==0||com[node]!=-1)return ;
         com[node]=com_value;
         for(auto i: m[node]){
-            p[com_value].insert(i.second);
+            p[com_value]=p[com_value]&i.second;
             dfs(i.first,m,com_value,com,p);
         }
     }
@@ -16,7 +16,8 @@ public:
             m[edges[i][1]].push_back({edges[i][0],edges[i][2]});   
         }
         vector<int>com(n,-1);
-        map<int,set<int>>p;
+        vector<int>p(n,INT_MAX);
+        vector<int>ans;
         int com_value=1;
         for(int i=0;i<n;i++){
             if(com[i]==-1){
@@ -24,14 +25,9 @@ public:
             }
             com_value++;
         }
-        vector<int>ans;
         for(auto q: query){ 
             if(com[q[0]]==com[q[1]]&& com[q[0]]!=-1){
-                int tem=INT_MAX;
-                for(auto i: p[com[q[0]]]){
-                    tem=tem&i;
-                }
-                ans.push_back(tem);
+                ans.push_back(p[com[q[0]]]);
             }
             else{
                 ans.push_back(-1);
