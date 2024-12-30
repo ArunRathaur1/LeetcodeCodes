0724f4a3dd123aba54fff5a2678258c08a4baf13
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2024-12-30 19:16:59 +0530

    Time: 137 ms (34.1%), Space: 255.8 MB (24.89%) - LeetHub

--- 3348-minimum-cost-walk-in-weighted-graph/3348-minimum-cost-walk-in-weighted-graph.cpp
+++ 3348-minimum-cost-walk-in-weighted-graph/3348-minimum-cost-walk-in-weighted-graph.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void dfs(int node,map<int,vector<pair<int,int>>>&m,int &com_value,vector<int>&com,vector<int>&p){
+    void dfs(int node,vector<vector<pair<int,int>>>&m,int &com_value,vector<int>&com,vector<int>&p){
         if(m[node].size()==0||com[node]!=-1)return ;
         com[node]=com_value;
         for(auto i: m[node]){
@@ -9,7 +9,7 @@ public:
         }
     }
     vector<int> minimumCost(int n, vector<vector<int>>& edges, vector<vector<int>>& query) {
-        map<int,vector<pair<int,int>>>m;
+        vector<vector<pair<int,int>>>m(n);
         int t=edges.size();
         for(int i=0;i<t;i++){
             m[edges[i][0]].push_back({edges[i][1],edges[i][2]});
