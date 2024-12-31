cbc09edd5cd5ea2a9bb26c61821002a9dc1c8cb5
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2024-12-31 13:07:33 +0530

    Time: 4 ms (6.16%), Space: 19 MB (41.48%) - LeetHub

--- 0983-validate-stack-sequences/0983-validate-stack-sequences.cpp
+++ 0983-validate-stack-sequences/0983-validate-stack-sequences.cpp
@@ -1,27 +1,17 @@
 class Solution {
 public:
     bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
-      int p1=0;
-      int p2=0;
-      int l1=pushed.size();
-      int l2=popped.size();
-      stack<int>st;
-      while(p1!=l1&&p2!=l2){
-        while(p1<l1&&popped[p2]!=pushed[p1]){
-            st.push(pushed[p1]);
-            p1++;
+        stack<int>st;
+        int tem=0;
+        int x=popped.size();
+        for(auto i: pushed){
+            st.push(i);
+            while(!st.empty()&& tem<x&& st.top()==popped[tem]){
+                st.pop();
+                tem++;
+            }
         }
-        p2++;
-        while(p2<l2 &&!st.empty()&& st.top()==popped[p2]){
-            st.pop();
-            p2++;
-        }
-        p1++;
-      }
-      if(st.empty()){
-        return true;
-      }
-      return false;
-
+        if(st.empty())return true;
+        return false;
     }
 };
