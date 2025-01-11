60a1864d1020df96c1c3c07227c444000aa1dbc4
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2025-01-11 11:42:07 +0530

    Time: 27 ms (14.34%), Space: 83.1 MB (5.17%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool answer(string s1, string s2,string s3,int i1,int i2,int i3,int n1,int n2,int n3,vector<vector<vector<int>>>&dp){
+    bool answer(string& s1, string& s2,string& s3,int i1,int i2,int i3,int& n1,int& n2,int& n3,vector<vector<vector<int>>>&dp){
         if(i1==n1&& i2==n2&& i3==n3)return true;
         if(i1>n1||i2>n2||i3>n3)return false;
         if(dp[i1][i2][i3]!=-1)return (dp[i1][i2][i3]==1?true:false);
@@ -23,6 +23,6 @@ public:
         int n2=s2.length();
         int n3=s3.length();
         vector<vector<vector<int>>>dp(n1+1,vector<vector<int>>(n2+1,vector<int>(n3+1,-1)));
-        return answer(s1,s2,s3,0,0,0,s1.length(),s2.length(),s3.length(),dp);
+        return answer(s1,s2,s3,0,0,0,n1,n2,n3,dp);
     }
 };
