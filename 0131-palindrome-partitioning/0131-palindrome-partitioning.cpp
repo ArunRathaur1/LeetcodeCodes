a8818a19edee23f37a63581bc80c0a869647337b
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2025-01-11 20:50:20 +0530

    Time: 55 ms (55.75%), Space: 57.8 MB (56.66%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
-    void allparts(string s ,int index,vector<string>&ans,int n,vector<vector<string>>&ar){
+    void allparts(string s ,int index,vector<string>&ans,int n,vector<vector<string>>&ar,map<pair<int,int>,int>&dp){
         if(index>n)return;
         if(index==n){
             ar.push_back(ans);
         }
         for(int i=index;i<n;i++){
-            int left=index;
-            int right=i;
-            int flag=0;
-            while(left<right){
+            if(dp.find({i+1,index+1})==dp.end()){
+                int left=index;
+                int right=i;
+                int flag=0;
+                while(left<right){
                 if(s[left]==s[right]){
                     left++;
                     right--;
@@ -17,12 +18,15 @@ public:
                 else{
                     flag=1;
                     break;
+                    }
                 }
+                dp[{(i+1),(index+1)}]=flag==0?1:0;
+
             }
-            if(flag==0){
+            if(dp[{(i+1),(index+1)}]==1){
                 string tem=s.substr(index,i-index+1);
                 ans.push_back(tem);
-                allparts(s,i+1,ans,n,ar);
+                allparts(s,i+1,ans,n,ar,dp);
                 ans.pop_back();
             }
         }
@@ -31,7 +35,8 @@ public:
         vector<vector<string>>ar;
         vector<string>ans;
         int l=s.length();
-        allparts(s,0,ans,l,ar);
+        map<pair<int,int>,int>dp;
+        allparts(s,0,ans,l,ar,dp);
         return ar;
     }
 };
