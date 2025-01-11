958e195d3d599794ca117a58facc1534b4f8a62f
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2025-01-11 20:24:33 +0530

    Time: 0 ms (100%), Space: 13.9 MB (87.96%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -11,12 +11,13 @@ public:
     void solve(vector<vector<char>>& board) {
         int row=board.size();
         int col=board[0].size();
+        for(int i=0;i<col;i++){
+            dfs(board,0,i,row,col);
+            dfs(board,row-1,i,row,col);
+        }
         for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                if(board[i][j]=='O'&&(i==0||i==row-1||j==0||j==col-1)){
-                   dfs(board,i,j,row,col); 
-                }
-            }
+            dfs(board,i,0,row,col);
+            dfs(board,i,col-1,row,col);
         }
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
