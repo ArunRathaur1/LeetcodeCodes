01ffd1dff4a2af2e1c87c6f230b02ede67ed4c7c
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2025-01-11 13:48:35 +0530

    Time: 17 ms (21.82%), Space: 19.3 MB (20.46%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
@@ -1,27 +1,40 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
 class Solution {
 public:
     Node* connect(Node* root) {
-        if (!root) return NULL;
-
-        queue<Node*> q;
+        if(root==NULL)return NULL;
+        queue<Node*>q;
         q.push(root);
-
-        while (!q.empty()) {
-            int size = q.size(); // Number of nodes at the current level
-            Node* prev = NULL;
-
-            for (int i = 0; i < size; i++) {
-                Node* curr = q.front();
+        while(!q.empty()){
+            int s=q.size();
+            Node* prev=NULL;
+            for(int i=0;i<s;i++){
+                Node* tem=q.front();
                 q.pop();
+                if(prev)prev->next=tem;
+                prev=tem;
+                if(tem->left)q.push(tem->left);
+                if(tem->right)q.push(tem->right);
 
-                if (prev) prev->next = curr; // Connect the previous node to the current node
-                prev = curr;
-
-                if (curr->left) q.push(curr->left);
-                if (curr->right) q.push(curr->right);
             }
         }
-
         return root;
     }
 };
