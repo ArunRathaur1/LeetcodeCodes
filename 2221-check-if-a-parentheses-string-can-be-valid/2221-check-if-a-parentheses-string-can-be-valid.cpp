926d7533a59a9c66ed22e3aa22410d828e9079ac
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2025-01-12 11:54:30 +0530

    Time: 7 ms (95.69%), Space: 30.2 MB (53.05%) - LeetHub

--- 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.cpp
+++ 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.cpp
@@ -1,38 +1,26 @@
 class Solution {
 public:
     bool canBeValid(string s, string locked) {
-        stack<int>ar1,ar2;
+        if(s.length()%2!=0)return false;
+        int opening=0;
+        int closing=0;
+        int wildcard=0;
         int n=s.length();
         for(int i=0;i<n;i++){
-            if(s[i]=='('&& locked[i]=='1'){
-                ar1.push(i);
-            }
-            else if(locked[i]=='0'){
-                ar2.push(i);
-            }
-            else{
-                if(!ar1.empty()){
-                    ar1.pop();
-                }
-                else if(!ar2.empty()){
-                    ar2.pop();
-                }
-                else{
-                    return false;
-                }
-            }
+            if(locked[i]=='0')wildcard++;
+            else if(s[i]=='(')opening++;
+            else if(s[i]==')')closing++;
+            if(wildcard+opening<closing)return false;
         }
-        while(!ar1.empty()&& !ar2.empty()){
-            if(ar1.top()<ar2.top()){
-                ar1.pop();
-                ar2.pop();
-            }
-            else{
-                return false;
-            }
+        opening=0;
+        closing=0;
+        wildcard=0;
+        for(int i=n-1;i>=0;i--){
+            if(locked[i]=='0')wildcard++;
+            else if(s[i]=='(')opening++;
+            else if(s[i]==')')closing++;
+            if(wildcard+closing<opening)return false;
         }
-        if(!ar1.empty())return false;
-        if(ar2.size()%2==0)return true;
-        return false;
+        return true;
     }
 };
