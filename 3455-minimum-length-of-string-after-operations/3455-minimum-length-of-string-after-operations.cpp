ada713cfe5caebc85816fb8b96872c35fba5b4a6
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2025-01-13 22:06:17 +0530

    Time: 13 ms (71.77%), Space: 32 MB (74.19%) - LeetHub

--- 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.cpp
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.cpp
@@ -1,37 +1,23 @@
 class Solution {
 public:
     int minimumLength(string s) {
-        int length = s.length();
-        int ans = length;
-        
-        for (int i = 0; i < length; i++) {
-            if (s[i] == 'A') continue;
-            
-            int n1 = -1;
-            for (int j = i-1; j>=0; j--) {
-                if (s[i] == s[j]) {
-                    n1 = j;
-                    break;
+       vector<int>ar(26,0);
+       int l=s.length();
+       for(int i=0;i<l;i++){
+            ar[s[i]-'a']++;
+       } 
+       int count=0;
+       for(int i=0;i<26;i++){
+            if(ar[i]<=2)count=count+ar[i];
+            else{
+                if(ar[i]%2==0){
+                    count=count+2;
+                }
+                else{
+                    count++;
                 }
             }
-            if (n1 == -1) continue;
-            
-            int n2 = -1;
-            for (int j = i + 1; j < length; j++) {
-                if (s[i] == s[j]) {
-                    n2 = j;
-                    break;
-                }
-            }
-            if (n2 == -1) continue;
-            
-            if (n1 != -1 && n2 != -1) {
-                ans -= 2;
-                s[n1] = 'A';
-                s[n2] = 'A';
-            }
-        }
-        
-        return ans;
+       }
+       return count;
     }
 };
