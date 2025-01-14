a67e8ec900a281f24f3c498e45d06e598aa73b3b
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2025-01-14 20:17:56 +0530

    Time: 2 ms (10.26%), Space: 8.7 MB (22.77%) - LeetHub

---
+++ 0165-compare-version-numbers/0165-compare-version-numbers.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+        stringstream ss(version1), ss1(version2);
+        vector<string>ar1;
+        vector<string>ar2;
+        string g1,g2;
+        while(!ss.eof()||!ss1.eof()){
+            int num1=0;
+            int num2=0;
+            if(getline(ss,g1,'.')){
+                num1=stoi(g1);
+            }
+            if(getline(ss1,g2,'.')){
+                num2=stoi(g2);
+            }
+            if(num1>num2)return 1;
+            else if(num2>num1)return -1; 
+        }
+        return 0;
+    }
+};
