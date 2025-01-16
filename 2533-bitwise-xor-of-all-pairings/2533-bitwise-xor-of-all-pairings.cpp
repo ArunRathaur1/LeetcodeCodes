6c74e3d4428996bd720f15997654b38e94f129cd
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2025-01-16 19:16:59 +0530

    Time: 0 ms (100%), Space: 64.3 MB (64.63%) - LeetHub

---
+++ 2533-bitwise-xor-of-all-pairings/2533-bitwise-xor-of-all-pairings.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
+        int x1=nums1.size();
+        int x2=nums2.size();
+        cout<<x1<<" "<<x2<<endl;
+        int absxor=0;
+        if(x1%2==0&&x2%2==0)return 0;
+        int first=0,second=0;
+        for(int i=0;i<x1;i++){
+            first=first^nums1[i];
+        }
+        for(int i=0;i<x2;i++){
+            second=second^nums2[i];
+        }
+        cout<<first<<" "<<second<<endl;
+        if(x1%2==0){
+            return first;
+        }
+        else if(x2%2==0){
+            return second;
+        }
+        else{
+            return first^second;
+        }
+    }
+};
