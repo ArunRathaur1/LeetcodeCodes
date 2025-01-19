0cfaf95575f331651e485ab4906d4cfaab10caeb
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2025-01-19 17:06:48 +0530

    Time: 0 ms (100%), Space: 18.6 MB (47.56%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,67 +1,54 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int row = matrix.size();
-        int col = matrix[0].size();
+        int row=matrix.size();
+        int col=matrix[0].size();
+        int flag1=0;
+        int flag2=0;
+        for(int i=0;i<row;i++){
+            if(matrix[i][0]==0){
+                flag1=1;
+                break;
+            }
+        }
+        for(int i=0;i<col;i++){
+            if(matrix[0][i]==0){
+                flag2=1;
+                break;
+            }
+        }
+        for(int i=1;i<row;i++){
+            for(int j=1;j<col;j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                }
+            }
+        }
+        for(int i=1;i<row;i++){
+            if(matrix[i][0]==0){
+                for(int j=0;j<col;j++){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        for(int i=0;i<col;i++){
+            if(matrix[0][i]==0){
+                for(int j=0;j<row;j++){
+                    matrix[j][i]=0;
+                }
+            }
+        }
+        if(flag1==1){
+            for(int i=0;i<row;i++){
+                matrix[i][0]=0;
+            }
+        }
+        if(flag2==1){
+            for(int i=0;i<col;i++){
+                matrix[0][i]=0;
+            }
+        }
         
-        // Flags to check if the first row and first column need to be zeroed
-        bool flagRow = false;
-        bool flagCol = false;
-
-        // Check if the first column needs to be zeroed
-        for (int i = 0; i < row; i++) {
-            if (matrix[i][0] == 0) {
-                flagCol = true;
-                break;
-            }
-        }
-
-        // Check if the first row needs to be zeroed
-        for (int j = 0; j < col; j++) {
-            if (matrix[0][j] == 0) {
-                flagRow = true;
-                break;
-            }
-        }
-
-        // Use the first row and column as markers
-        for (int i = 1; i < row; i++) {
-            for (int j = 1; j < col; j++) {
-                if (matrix[i][j] == 0) {
-                    matrix[i][0] = 0;
-                    matrix[0][j] = 0;
-                }
-            }
-        }
-
-        // Zero out cells based on markers in the first row and column
-        for (int i = 1; i < row; i++) {
-            if (matrix[i][0] == 0) {
-                for (int j = 1; j < col; j++) {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-
-        for (int j = 1; j < col; j++) {
-            if (matrix[0][j] == 0) {
-                for (int i = 1; i < row; i++) {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-
-        // Handle the first row and first column separately
-        if (flagRow) {
-            for (int j = 0; j < col; j++) {
-                matrix[0][j] = 0;
-            }
-        }
-
-        if (flagCol) {
-            for (int i = 0; i < row; i++) {
-                matrix[i][0] = 0;
-            }
-        }
     }
 };
