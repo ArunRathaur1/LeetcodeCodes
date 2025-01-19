bc6e61205ded058d6c247e322f27d1aad2e77c56
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2025-01-19 15:58:22 +0530

    Time: 0 ms (100%), Space: 13.8 MB (91.8%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -4,20 +4,13 @@ public:
         long long x=0;
         for(auto i: nums)x=x^i;
         x=(x&(x-1))^x;
-        vector<int>bucket1,bucket2;
-        for(auto i: nums){
-            if((i&x)!=0)bucket1.push_back(i);
-            else{
-                bucket2.push_back(i);
-            }
-        }
         int num1=0;
         int num2=0;
-        for(auto i: bucket1){
-            num1=num1^i;
-        }
-        for(auto i: bucket2){
-            num2=num2^i;
+        for(auto i: nums){
+            if((i&x)!=0)num1=num1^i;
+            else{
+                num2=num2^i;
+            }
         }
         return {num1,num2};
     }
