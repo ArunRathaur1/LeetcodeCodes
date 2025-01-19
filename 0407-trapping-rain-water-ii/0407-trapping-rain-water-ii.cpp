ad54d647101ee1cf15ebea6cd7725c6abfd5d003
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2025-01-19 11:39:10 +0530

    Time: 18 ms (96.01%), Space: 17.2 MB (84.3%) - LeetHub

---
+++ 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<vector<int>>direction={{0,1},{0,-1},{1,0},{-1,0}};
+    int trapRainWater(vector<vector<int>>& heightMap) {
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> q;
+        int row=heightMap.size();
+        int col=heightMap[0].size();
+        int totalwater=0;
+        vector<vector<bool>> visited(row, vector<bool>(col, false));
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(i==0||j==0||i==row-1||j==col-1){
+                    visited[i][j]=true;
+                    q.push({heightMap[i][j],{i,j}});
+                }
+            }
+        }
+        while(!q.empty()){
+            pair<int,pair<int,int>>x=q.top();
+            q.pop();
+            int value=x.first;
+            int r=x.second.first;
+            int c=x.second.second;
+            for(int i=0;i<4;i++){
+                int newx=r+direction[i][0];
+                int newy=c+direction[i][1];
+                if(newx>=row|| newy>=col|| newx<0|| newy<0 )continue;
+                if(visited[newx][newy]==false){
+                    visited[newx][newy]=true;
+                    if(heightMap[newx][newy]<value){
+                        totalwater+=-heightMap[newx][newy]+value;
+                    }
+                    int p=max(heightMap[newx][newy],value);
+                    q.push({p,{newx,newy}});
+                }
+            }
+        }
+        return totalwater;
+    }
+};
