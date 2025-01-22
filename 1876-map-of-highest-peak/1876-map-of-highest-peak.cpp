a33695a5493560d3407ab5bbbec450d1f4eb428a
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2025-01-22 08:22:20 +0530

    Time: 252 ms (15.65%), Space: 103.1 MB (95.51%) - LeetHub

---
+++ 1876-map-of-highest-peak/1876-map-of-highest-peak.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int>> highestPeak(vector<vector<int>>& isWater) {
+        int row=isWater.size();
+        int col=isWater[0].size();
+        vector<vector<int>>heights(row,vector<int>(col,INT_MAX));
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>q;
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(isWater[i][j]==1){
+                    heights[i][j]=0;
+                    q.push({0,{i,j}});
+                }
+            }
+        }
+        vector<vector<int>>direction={{-1,0},{1,0},{0,-1},{0,1}};
+        while(!q.empty()){
+           pair<int,pair<int,int>>x=q.top();
+           q.pop();
+           int height=x.first;
+           int r=x.second.first;
+           int c=x.second.second;
+           for(int i=0;i<4;i++){
+            int newx=r+direction[i][0];
+            int newy=c+direction[i][1];
+            if(newx<0||newx>=row||newy<0||newy>=col)continue;
+            if(height+1<heights[newx][newy]){
+                heights[newx][newy]=height+1;
+                q.push({height+1,{newx,newy}});
+            }
+           }
+        }
+        return heights;
+    }
+};
