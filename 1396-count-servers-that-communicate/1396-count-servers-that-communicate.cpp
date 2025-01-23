a3bcdf1ed065af58be2319119a69ca7ad731d761
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2025-01-23 09:55:20 +0530

    Time: 4 ms (44.09%), Space: 26.2 MB (89.76%) - LeetHub

--- 1396-count-servers-that-communicate/1396-count-servers-that-communicate.cpp
+++ 1396-count-servers-that-communicate/1396-count-servers-that-communicate.cpp
@@ -1,36 +1,33 @@
 class Solution {
 public:
-    void connected(vector<vector<int>>&grid,int row,int col,int i,int j,int &x){
-        if(i<0||j<0||i>=row||j>=col||grid[i][j]==2)return;
-        if(grid[i][j]==1){
-            grid[i][j]=2;
-            x++;
-            for(int r=0;r<row;r++){
-                if(grid[r][j]==1){
-                    connected(grid,row,col,r,j,x);
-                }
-            }
-            for(int c=0;c<col;c++){
-                if(grid[i][c]==1){
-                    connected(grid,row,col,i,c,x);
-                }
-            }
-        }
-    }
     int countServers(vector<vector<int>>& grid) {
         int count=0;
         int row=grid.size();
         int col=grid[0].size();
+        vector<int>rownumber;
+        vector<int>colnumber;
+        for(int i=0;i<col;i++){
+            int temsum=0;
+            for(int j=0;j<row;j++){
+                temsum=temsum+grid[j][i];
+            }
+            rownumber.push_back(temsum);
+        }
+        for(int i=0;i<row;i++){
+            int temsum=0;
+            for(int j=0;j<col;j++){
+                    temsum=temsum+grid[i][j];
+            }
+            colnumber.push_back(temsum);
+        }
+        int total=0;
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
-                if(grid[i][j]==1){
-                    int x=0;
-                    connected(grid,row,col,i,j,x);
-                    if(x>=2)
-                    count=count+x;
+                if (grid[i][j] == 1 && (rownumber[j] > 1 || colnumber[i] > 1)) {
+                    total++;
                 }
             }
         }
-        return count;
+        return total;
     }
 };
