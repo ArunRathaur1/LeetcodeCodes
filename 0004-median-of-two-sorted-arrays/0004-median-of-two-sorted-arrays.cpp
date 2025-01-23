129c779227bdf9430a8a02906a32c4135fc1c7de
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2025-01-23 18:15:18 +0530

    Time: 0 ms (100%), Space: 95.1 MB (63.38%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,39 +1,36 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int s1=nums1.size();
-        int s2=nums2.size();
-        int prev=-1;
-        int curr=-1;
-        int i1=0;
-        int i2=0;
-        int index=(s1+s2)/2;
-        while(i1<s1&& i2<s2&& (i1+i2)<=index){
-            prev=curr;
-            if(nums1[i1]>nums2[i2]){
-                curr=nums2[i2];
-                i2++;
+        int n1=nums1.size();
+        int n2=nums2.size();
+        if(n2<n1) return findMedianSortedArrays(nums2,nums1);
+        int total=(n1+n2+1)/2;
+        int low=0;
+        int heigh=n1;
+        while(low<=heigh){
+            int mid1=(low+heigh)/2;
+            int mid2=total-mid1;
+            int l1=INT_MIN,l2=INT_MIN;
+            int r1=INT_MAX,r2=INT_MAX;
+            if(mid1<n1)r1=nums1[mid1];
+            if(mid2<n2)r2=nums2[mid2];
+            if(mid1-1>=0)l1=nums1[mid1-1];
+            if(mid2-1>=0)l2=nums2[mid2-1];
+            if(l2<=r1&& r2>=l1){
+                if((n1+n2)%2==0){
+                    return double(max(l1,l2)+min(r1,r2))/2.0;
+                }
+                else{
+                    return (double)max(l1,l2);
+                }
+            }
+            else if(l2>r1){
+                low=mid1+1;
             }
             else{
-                curr=nums1[i1];
-                i1++;
+                heigh=mid1-1;
             }
         }
-        while(i1<s1 && (i1+i2)<=index){
-            prev=curr;
-            curr=nums1[i1];
-            i1++;
-        }
-        while(i2<s2&& (i1+i2)<=index){
-            prev=curr;
-            curr=nums2[i2];
-            i2++;
-        }
-        cout<<prev<<" "<<curr<<endl;
-
-        if((s1+s2)%2==0){
-            return (float(prev+curr))/2;
-        }
-        return float(curr);
+        return -1;
     }
 };
