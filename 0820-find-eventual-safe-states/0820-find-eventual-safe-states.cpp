2f245781c3404974216f93585f396e9be4228038
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2025-01-24 10:10:20 +0530

    Time: 22 ms (65.36%), Space: 55 MB (64.28%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -1,38 +1,38 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>>&adj,int node,unordered_set<int>&st,vector<bool>&visited){
+    bool dfs(vector<vector<int>>&graph,int node,set<int>&st,vector<bool>&visited){
         if(st.find(node)!=st.end())return true;
         if(visited[node]==true)return false;
         visited[node]=true;
-        if(adj[node].size()==0){
+        if(graph[node].size()==0){
             st.insert(node);
             return true;
         }
         bool p=true;
-        for(auto i: adj[node]){
-            bool x=dfs(adj,i,st,visited);
+        for(auto i: graph[node]){
+            bool x=dfs(graph,i,st,visited);
+            if(x==false)return false;
             p=p&x;
         }
-       if(p==true){
-        st.insert(node);
-        return true;
-       }
-       return false;
+        if(p==true){
+            st.insert(node);
+            return true;
+        }
+        return false;
     }
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        unordered_set<int>st;
         int s=graph.size();
-        vector<bool>visited(s,false);
-        for(int i=0;i<s;i++){
-            if(visited[i]==false){
-                bool x=dfs(graph,i,st,visited);
-            }
-        }
+        set<int>st;
         vector<int>ar;
-        for(auto i: st){
-            ar.push_back(i);
+       vector<bool>visited(s,false);
+       for(int i=0;i<s;i++){
+        if(visited[i]==false){
+           bool x= dfs(graph,i,st,visited);
         }
-        sort(ar.begin(),ar.end());
-        return ar;
+       } 
+       for(auto i: st){
+        ar.push_back(i);
+       }
+       return ar;
     }
 };
