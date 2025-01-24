946c4aebeb77ed9fcfd5789f57a35353559ac5e8
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2025-01-24 13:09:20 +0530

    Time: 0 ms (100%), Space: 50.4 MB (39.92%) - LeetHub

--- 2047-find-a-peak-element-ii/2047-find-a-peak-element-ii.cpp
+++ 2047-find-a-peak-element-ii/2047-find-a-peak-element-ii.cpp
@@ -3,31 +3,29 @@ public:
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
         int row=mat.size();
         int col=mat[0].size();
-        int left=0;
-        int right=col-1;
-        while(left<=right){
-            int mid=(left+right)/2;
-            int maxelement=0;
-            int index=-1;
-            for(int i=0;i<row;i++){
-               if(maxelement<mat[i][mid]){
-                maxelement=mat[i][mid];
+       int left=0;
+       int right=col-1;
+       while(left<=right){
+        int mid=(left+right)/2;
+        int index=0;
+        for(int i=0;i<row;i++){
+            if(mat[i][mid]>mat[index][mid]){
                 index=i;
-               }
-            }
-            int x3=mid+1<col?mat[index][mid+1]:-1;//right
-            int x4=mid-1>=0?mat[index][mid-1]:-1;//left
-            if(mat[index][mid]>x3&& mat[index][mid]>x4)
-            {
-                return {index,mid};
-            }
-            else if(mat[index][mid]<x4){
-                right=mid-1;
-            }
-            else{
-               left=mid+1;
             }
         }
-        return {-1,-1};
+        //mat[index][mid]hieght in that colum
+        int leftside=mid-1>=0?mat[index][mid-1]:-1;
+        int rightside=mid+1<col?mat[index][mid+1]:-1;
+        if(mat[index][mid]>leftside&& mat[index][mid]>rightside){
+            return {index,mid};
+        }
+        else if(mat[index][mid]>leftside){
+            left=mid+1;
+        }
+        else{
+            right=mid-1;
+        }
+       } 
+       return {-1,-1};
     }
 };
