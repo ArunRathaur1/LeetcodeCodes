58b8918736831208c944ce76b6c9b6e58dadb824
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2025-01-24 10:12:07 +0530

    Time: 45 ms (58.57%), Space: 55 MB (64.28%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -1,38 +1,40 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>>&graph,int node,set<int>&st,vector<bool>&visited){
-        if(st.find(node)!=st.end())return true;
-        if(visited[node]==true)return false;
-        visited[node]=true;
-        if(graph[node].size()==0){
+    bool dfs(vector<vector<int>>& graph, int node, set<int>& st,vector<bool>& visited) {
+        if (st.find(node) != st.end())
+            return true;
+        if (visited[node] == true)
+            return false;
+        visited[node] = true;
+        if (graph[node].size() == 0) {
             st.insert(node);
             return true;
         }
-        bool p=true;
-        for(auto i: graph[node]){
-            bool x=dfs(graph,i,st,visited);
-            if(x==false)return false;
-            p=p&x;
+        bool p = true;
+        for (auto i : graph[node]) {
+            bool x = dfs(graph, i, st, visited);
+            if (x == false)
+                return false;
+            p = p & x;
         }
-        if(p==true){
+        if (p == true) {
             st.insert(node);
             return true;
         }
         return false;
     }
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int s=graph.size();
-        set<int>st;
-        vector<int>ar;
-       vector<bool>visited(s,false);
-       for(int i=0;i<s;i++){
-        if(visited[i]==false){
-           bool x= dfs(graph,i,st,visited);
+        int s = graph.size();
+        set<int> st;
+        vector<int> ar;
+        vector<bool> visited(s, false);
+        for (int i = 0; i < s; i++) {
+            if (visited[i] == false) {
+                bool x = dfs(graph, i, st, visited);
+                if(x==true)ar.push_back(i);
+            }
+            else if(st.find(i)!=st.end())ar.push_back(i);
         }
-       } 
-       for(auto i: st){
-        ar.push_back(i);
-       }
-       return ar;
+        return ar;
     }
 };
