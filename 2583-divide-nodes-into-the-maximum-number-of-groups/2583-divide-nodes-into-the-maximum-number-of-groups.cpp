b84581215d97e1f622b8e54e53e878d05d8b5c6a
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2025-01-30 17:13:22 +0530

    Time: 163 ms (41.18%), Space: 44.4 MB (74.37%) - LeetHub

--- 2583-divide-nodes-into-the-maximum-number-of-groups/2583-divide-nodes-into-the-maximum-number-of-groups.cpp
+++ 2583-divide-nodes-into-the-maximum-number-of-groups/2583-divide-nodes-into-the-maximum-number-of-groups.cpp
@@ -1,117 +1,106 @@
-class DSU {
-public:
-    vector<int> parent, rank;
-    
-    DSU(int n) {
-        parent.resize(n);
-        rank.resize(n, 1);
-        for (int i = 0; i < n; i++) {
-            parent[i] = i;
+class DSU{
+    public:
+    vector<int>parent,rank;
+    DSU(int nodes){
+        parent.resize(nodes);
+        rank.resize(nodes);
+        for(int i=0;i<nodes;i++ ){
+            parent[i]=i;
+            rank[i]=0;
         }
     }
-    
-    int find(int x) {
-        if (parent[x] != x) {
-            parent[x] = find(parent[x]);  // Path compression
+    vector<int> componets(){
+        set<int>st;
+        for(auto i: parent){
+            st.insert(i);
         }
-        return parent[x];
+        vector<int>ar(st.begin(),st.end());
+        return ar;
     }
-    
-    void unite(int x, int y) {
-        int rootX = find(x);
-        int rootY = find(y);
-        if (rootX != rootY) {
-            if (rank[rootX] > rank[rootY]) {
-                parent[rootY] = rootX;
-            } else if (rank[rootX] < rank[rootY]) {
-                parent[rootX] = rootY;
-            } else {
-                parent[rootY] = rootX;
-                rank[rootX]++;
-            }
+    int findparent(int x){
+        if(parent[x]==x)return x;
+        return parent[x]=findparent(parent[x]);
+    }
+    void joinedge(int x,int y){
+        int x1=findparent(x);
+        int y1=findparent(y);
+        if(rank[x1]>rank[y1]){
+            parent[y1]=x1;
+        }
+        else if(rank[x1]<rank[y1]){
+            parent[x1]=y1;
+        }
+        else{
+            parent[x1]=y1;
+            rank[y1]++;
         }
     }
 };
-
 class Solution {
 public:
-    bool isBipartite(vector<vector<int>>& adj, int n, vector<int>& color) {
-        queue<int> q;
-        
-        for (int i = 0; i < n; i++) {
-            if (color[i] == -1) {
-                q.push(i);
-                color[i] = 0;
-
-                while (!q.empty()) {
-                    int node = q.front();
-                    q.pop();
-
-                    for (int neighbor : adj[node]) {
-                        if (color[neighbor] == -1) {
-                            color[neighbor] = 1 - color[node];
-                            q.push(neighbor);
-                        } else if (color[neighbor] == color[node]) {
-                            return false;
+    bool dfs(vector<int>&colors,int colorvalue,int node,vector<vector<int>>&adj){
+        colorvalue=1-colorvalue;
+        colors[node]=colorvalue;
+        for(auto i: adj[node]){
+            if(colors[i]==-1){
+                bool x=dfs(colors,colorvalue,i,adj);
+                if(x==false)return false;
+            }
+            else if(colors[node]==colors[i])return false;
+        }
+        return true;
+    }
+    int magnificentSets(int n, vector<vector<int>>& edges) {
+        vector<vector<int>>adj(n+1);
+        DSU ob(n+1);
+        for(auto i: edges){
+            ob.joinedge(i[0],i[1]);
+            adj[i[0]].push_back(i[1]);
+            adj[i[1]].push_back(i[0]);
+        }
+        vector<int>colors(n+1,-1);
+        int colorvalue=0;
+        for(int i=1;i<=n;i++){
+            if(colors[i]==-1){
+                bool x=dfs(colors,colorvalue,i,adj);
+                if(x==false)return -1;
+            }
+        } 
+        vector<int>componets=ob.componets();
+        map<int,int>m;
+        for(auto i: componets){
+            m[i]=0;
+        }
+        for(int i=1;i<=n;i++){
+            vector<bool>visited(n+1,false);
+            queue<int>q;
+            q.push(i);
+            q.push(-1);
+            int count=0;
+            while(!q.empty()){
+                int t=q.front();
+                q.pop();
+                if(t==-1){
+                    count++;
+                    if(!q.empty())q.push(-1);
+                }
+                else{
+                    visited[t]=true;
+                    for(auto j: adj[t]){
+                        if(visited[j]==false){
+                            visited[j]=true;
+                            q.push(j);
                         }
                     }
                 }
             }
+            int x=ob.findparent(i);
+            m[x]=max(m[x],count);
         }
-        return true;
-    }
-
-    int getMaxLayer(int start, vector<vector<int>>& adj, int n) {
-        queue<int> q;
-        vector<int> dist(n, -1);
-        
-        q.push(start);
-        dist[start] = 0;
-        int maxDist = 0;
-
-        while (!q.empty()) {
-            int node = q.front();
-            q.pop();
-
-            for (int neighbor : adj[node]) {
-                if (dist[neighbor] == -1) {
-                    dist[neighbor] = dist[node] + 1;
-                    q.push(neighbor);
-                    maxDist = max(maxDist, dist[neighbor]);
-                }
-            }
+        int total=0;
+        for(auto i: m){
+            total+=i.second;
         }
-        return maxDist + 1;
-    }
-
-    int magnificentSets(int n, vector<vector<int>>& edges) {
-        vector<vector<int>> adj(n);
-        DSU dsu(n);
-
-        for (auto& edge : edges) {
-            int u = edge[0] - 1, v = edge[1] - 1; // Assuming 1-based input
-            adj[u].push_back(v);
-            adj[v].push_back(u);
-            dsu.unite(u, v);
-        }
-
-        vector<int> color(n, -1);
-        if (!isBipartite(adj, n, color)) return -1;
-
-        unordered_map<int, vector<int>> components;
-        for (int i = 0; i < n; i++) {
-            components[dsu.find(i)].push_back(i);
-        }
-
-        int totalCount = 0;
-        for (auto& [root, nodes] : components) {
-            int maxLayer = 0;
-            for (int node : nodes) {
-                maxLayer = max(maxLayer, getMaxLayer(node, adj, n));
-            }
-            totalCount += maxLayer;
-        }
-        
-        return totalCount;
+        return total;
     }
 };
