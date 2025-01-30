c4037fb3ef835905d2628bc73a87e6e298f9efcf
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2025-01-30 17:43:41 +0530

    Time: 16 ms (7.28%), Space: 13.3 MB (35.14%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -4,7 +4,7 @@ public:
         std::vector<std::pair<char, int>> x = {
             {'I', 1},   {'V', 5},   {'X', 10},  {'L', 50},
             {'C', 100}, {'D', 500}, {'M', 1000}};
-        map<char,int>m;
+        unordered_map<char,int>m;
         for(auto i: x){
             m[i.first]=i.second;
         }
