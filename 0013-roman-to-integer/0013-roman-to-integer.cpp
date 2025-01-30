a8285d656d65198cb7b82ad5a193bb98bbdcd3b0
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2025-01-30 17:45:42 +0530

    Time: 9 ms (35.7%), Space: 12.9 MB (52.2%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        std::vector<std::pair<char, int>> x = {
-            {'I', 1},   {'V', 5},   {'X', 10},  {'L', 50},
-            {'C', 100}, {'D', 500}, {'M', 1000}};
         unordered_map<char,int>m;
-        for(auto i: x){
-            m[i.first]=i.second;
-        }
+        m['I'] = 1;
+        m['V'] = 5;
+        m['X'] = 10;
+        m['L'] = 50;
+        m['C'] = 100;
+        m['D'] = 500;
+        m['M'] = 1000;
         int size=s.length();
         vector<int>ar(size,0);
         vector<int>gar(size,0);
