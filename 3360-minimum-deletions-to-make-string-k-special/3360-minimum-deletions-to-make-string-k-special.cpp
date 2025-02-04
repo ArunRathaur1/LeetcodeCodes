214793a8ad685b0fdbaddf43260b972ff1b597e2
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2025-02-05 01:34:35 +0530

    Time: 18 ms (32.66%), Space: 24.2 MB (5.03%) - LeetHub

--- 3360-minimum-deletions-to-make-string-k-special/3360-minimum-deletions-to-make-string-k-special.cpp
+++ 3360-minimum-deletions-to-make-string-k-special/3360-minimum-deletions-to-make-string-k-special.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
-    int solve(vector<int>&ar,int left,int right,int k){
+    int solve(vector<int>&ar,int left,int right,int k,vector<vector<int>>&dp){
         if(left>=right)return 0;
+        if(dp[left][right]!=-1)return dp[left][right];
         int case1=0,case2=0;
-        case1=ar[left]+solve(ar,left+1,right,k);
+        case1=ar[left]+solve(ar,left+1,right,k,dp);
         if(ar[right]-k-ar[left]>=0)
-        case2=ar[right]-k-ar[left]+solve(ar,left,right-1,k);
-        return min(case1,case2);
+        case2=ar[right]-k-ar[left]+solve(ar,left,right-1,k,dp);
+        return dp[left][right]=min(case1,case2);
     }
     int minimumDeletions(string word, int k) {
-        map<char,int>m;
+        unordered_map<char,int>m;
         int l=word.length();
         for(int i=0;i<l;i++){
             m[word[i]]++;
@@ -19,9 +20,10 @@ public:
             ar.push_back(i.second);
         }
         sort(ar.begin(),ar.end());
+        vector<vector<int>>dp(26,vector<int>(26,-1));
         int left=0;
         int right=ar.size()-1;
-        int c=solve(ar,left,right,k);
+        int c=solve(ar,left,right,k,dp);
         return c;
     }
 };
