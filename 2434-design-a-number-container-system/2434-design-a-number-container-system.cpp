d35d8a86fc7de75019d982e35a11ece4abc3cf9a
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2025-02-08 12:40:05 +0530

    Time: 160 ms (10%), Space: 198.1 MB (6.88%) - LeetHub

---
+++ 2434-design-a-number-container-system/2434-design-a-number-container-system.cpp
@@ -0,0 +1,31 @@
+class NumberContainers {
+public:
+    map<int,int>m1;
+    map<int,set<int>>m2;
+    NumberContainers() {
+        
+    }
+    
+    void change(int index, int number) {
+        if(m1.find(index)!=m1.end()){
+            m2[m1[index]].erase(index);
+            if(m2[m1[index]].size()==0)m2.erase(m1[index]);
+        }
+        m1[index]=number;
+        m2[number].insert(index);
+    }
+    
+    int find(int number) {
+        if(m2.find(number)!=m2.end()){
+            return *m2[number].begin();
+        }
+        return -1;
+    }
+};
+
+/**
+ * Your NumberContainers object will be instantiated and called as such:
+ * NumberContainers* obj = new NumberContainers();
+ * obj->change(index,number);
+ * int param_2 = obj->find(number);
+ */
