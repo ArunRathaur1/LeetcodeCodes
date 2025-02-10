2ebad3fe7541849eb71c226fc825805e0bf7286d
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2025-02-10 19:41:12 +0530

    Time: 2 ms (20.65%), Space: 9 MB (41.76%) - LeetHub

--- 3447-clear-digits/3447-clear-digits.cpp
+++ 3447-clear-digits/3447-clear-digits.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     string clearDigits(string s) {
-        stack<char>st;
-        int l=s.length();
-        for(int i=0;i<l;i++){
-            if(s[i]>='a'&&s[i]<='z'){
+        stack<char> st;
+        int l = s.length();
+        for (int i = 0; i < l; i++) {
+            if (s[i] >= 'a' && s[i] <= 'z') {
                 st.push(s[i]);
-            }
-            else{
+            } else {
                 st.pop();
             }
         }
-        string ans="";
-            while(!st.empty()){
-                ans+=st.top();
-                st.pop();
-            }
-            reverse(ans.begin(),ans.end());
-            return ans;
-            }
+        string ans = "";
+        while (!st.empty()) {
+            ans += st.top();
+            st.pop();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
 };
