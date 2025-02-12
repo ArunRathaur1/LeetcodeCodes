b0710caaa4314714ef6f6463f5c3fbb4424b1c9f
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2025-02-12 19:25:45 +0530

    Time: 11 ms (89.51%), Space: 63.8 MB (87.26%) - LeetHub

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.cpp
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.cpp
@@ -9,7 +9,7 @@ public:
         return sum;
     }
     int maximumSum(vector<int>& nums) {
-        vector<pair<int,int>>ar(100,{-1,-1});
+        vector<pair<int,int>>ar(85,{-1,-1});
         int p=nums.size();
         for(int i=0;i<p;i++){
             int x=digitsum(nums[i]);
@@ -30,7 +30,7 @@ public:
             }
         }
         long long maxsum=-1;
-        for(int i=0;i<100;i++){
+        for(int i=0;i<85;i++){
             if(ar[i].first!=-1&& ar[i].second!=-1){
                 maxsum=max(maxsum,1LL*ar[i].first+1LL*ar[i].second);
             }
