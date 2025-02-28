6ca8252b279b58c3e150ea1a98825bfdc274bcc7
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2025-02-28 17:10:00 +0530

    Time: 0 ms (100%), Space: 10.4 MB (69.43%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    vector<vector<int>> generate(vector<int>&nums,int index,int size){
-        if(index==size){
-            return {{}};
+    void subsests(vector<int>&nums,int index,int n,vector<int>&tem,vector<vector<int>>&ans){
+        ans.push_back(tem);
+        for(int i=index;i<n;i++){
+            if(i>index&& nums[i]==nums[i-1])continue;
+            tem.push_back(nums[i]);
+            subsests(nums,i+1,n,tem,ans);
+            tem.pop_back();
         }
-        vector<vector<int>>tem=generate(nums,index+1,size);
-        vector<vector<int>>newsubsets;
-        for(int i=0;i<tem.size();i++){
-            vector<int>k=tem[i];
-            k.push_back(nums[index]);
-            newsubsets.push_back(k);
-        }
-        tem.insert(tem.end(),newsubsets.begin(),newsubsets.end());
-        set<vector<int>>st(tem.begin(),tem.end());
-        vector<vector<int>>tem2(st.begin(),st.end());
-        return tem2;
+       
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>>ans;
+        vector<int>tem;
+        int n=nums.size();
+        int index=0;
         sort(nums.begin(),nums.end());
-        return generate(nums,0,nums.size());
+        subsests(nums,index,n,tem,ans);
+        return ans;
     }
 };
