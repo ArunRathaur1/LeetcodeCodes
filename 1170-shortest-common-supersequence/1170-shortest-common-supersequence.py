3915e7746543c7a311f11a8e71a7df166503ff7e
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2025-02-28 09:26:28 +0530

    Time: 623 ms (28.2%), Space: 34.7 MB (29.49%) - LeetHub

---
+++ 1170-shortest-common-supersequence/1170-shortest-common-supersequence.py
@@ -0,0 +1,43 @@
+class Solution(object):
+    def shortestCommonSupersequence(self, str1, str2):
+        """
+        :type str1: str
+        :type str2: str
+        :rtype: str
+        """
+        l1=len(str1)
+        l2=len(str2)
+        dp=[]
+        for i in range(l1+1):
+            p=[]
+            for j in range( l2+1):
+                p.append(0)
+            dp.append(p)
+        for i in range(1,l1+1):
+            for j in range(1,l2+1):
+                if str1[i-1]==str2[j-1]:
+                    dp[i][j]=dp[i-1][j-1]+1
+                else:
+                    dp[i][j]=max(dp[i][j-1],dp[i-1][j])
+        ans=""
+        d=l1
+        r=l2
+        while(d>0 and r>0):
+            if str1[d-1]==str2[r-1]:
+                ans+=str1[d-1]
+                d=d-1
+                r=r-1
+            elif dp[d][r-1]>dp[d-1][r]:
+                ans+=str2[r-1]
+                r=r-1
+            else:
+                ans+=str1[d-1]
+                d=d-1
+        while(d>0):
+            ans+=str1[d-1]
+            d=d-1
+        while(r>0):
+            ans+=str2[r-1]
+            r=r-1
+        ans=ans[::-1]
+        return ans
