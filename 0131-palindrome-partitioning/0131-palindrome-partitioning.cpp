15aa17f082644c8d368f014ddcf6f41469466490
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2025-02-28 18:59:34 +0530

    Time: 43 ms (62.41%), Space: 57.7 MB (53.64%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,42 +1,36 @@
 class Solution {
 public:
-    void allparts(string s ,int index,vector<string>&ans,int n,vector<vector<string>>&ar,vector<vector<int>>&dp){
-        if(index>n)return;
-        if(index==n){
-            ar.push_back(ans);
-        }
-        for(int i=index;i<n;i++){
-            if(dp[i+1][index+1]==-1){
-                int left=index;
-                int right=i;
-                int flag=0;
-                while(left<right){
-                if(s[left]==s[right]){
-                    left++;
-                    right--;
-                }
-                else{
-                    flag=1;
-                    break;
-                    }
-                }
-                dp[i+1][index+1]=flag==0?1:0;
-
+    bool check(string s){
+        int left=0;
+        int right=s.length()-1;
+        while(left<right){
+            if(s[left]==s[right]){
+                left++;
+                right--;
             }
-            if(dp[i+1][index+1]==1){
-                string tem=s.substr(index,i-index+1);
-                ans.push_back(tem);
-                allparts(s,i+1,ans,n,ar,dp);
-                ans.pop_back();
+            else return false;
+        }
+        return true;
+    }
+    void generate(string s,vector<string>&tem,vector<vector<string>>&ans,int index,int n){
+        if(index>=n){
+            ans.push_back(tem);
+            return;
+        }
+        for(int i=index+1;i<=n;i++){
+            string p=s.substr(index,i-index);
+            if(check(p)==true){
+                tem.push_back(p);
+                generate(s,tem,ans,i,n);
+                tem.pop_back();
             }
         }
     }
     vector<vector<string>> partition(string s) {
-        vector<vector<string>>ar;
-        vector<string>ans;
-        int l=s.length();
-        vector<vector<int>>dp(l+1,vector<int>(l+1,-1));
-        allparts(s,0,ans,l,ar,dp);
-        return ar;
+        vector<string>tem;
+        vector<vector<string>>ans;
+        int n=s.length();
+        generate(s,tem,ans,0,n);
+        return ans;
     }
 };
