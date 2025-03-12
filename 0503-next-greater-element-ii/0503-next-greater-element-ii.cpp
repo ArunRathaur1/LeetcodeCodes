b4b4968d20c0fcb4bc92bbde63486a0b43d98275
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2025-03-12 23:36:45 +0530

    Time: 5 ms (48.53%), Space: 30.5 MB (5.55%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,32 +1,21 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
-        vector<int>ar=nums;
-        for(auto I:nums)ar.push_back(I);
-        stack<int>nextGrater;
-        vector<int>ans;
-        int size=ar.size();
-        for(int i=size-1;i>=0;i--){
-            if(nextGrater.empty()){
-               nextGrater.push(ar[i]);
-               ans.push_back(-1);
+        int k=nums.size();
+        stack<int>st;
+        nums.insert(nums.end(),nums.begin(),nums.end());
+        int m=nums.size();
+        vector<int>tem(m,-1);
+        for(int i=m-1;i>=0;i--){
+            while(!st.empty()&& st.top()<=nums[i]){
+                st.pop();
             }
-            else{
-                while(!nextGrater.empty()&& nextGrater.top()<=ar[i])nextGrater.pop();
-                if(nextGrater.empty()){
-                    ans.push_back(-1);
-                }
-                else{
-                    ans.push_back(nextGrater.top());
-                }
-                nextGrater.push(ar[i]);
+            if(!st.empty()){
+                tem[i]=st.top();
             }
+            st.push(nums[i]);
         }
-        int index=ans.size()-1;
-        for(int i=0;i<nums.size();i++){
-            nums[i]=ans[index];
-            index--;
-        }
-        return nums;
+        vector<int>ans(tem.begin(),tem.begin()+k);
+        return ans;
     }
 };
