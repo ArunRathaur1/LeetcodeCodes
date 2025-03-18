a8c81191ddeec2ef0c247a59561f03ecda155567
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2025-03-18 19:49:08 +0530

    Time: 175 ms (5.44%), Space: 67.4 MB (5.03%) - LeetHub

--- 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.cpp
+++ 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
-    int countsubarray(vector<int>&nums,int k){
-        int count=0;
-        int size=nums.size();
-            unordered_map<int,int>m;
-            int left=0;
-            for(int j=0;j<size;j++){
-                m[nums[j]]++;
-                while(m.size()>k){
-                    m[nums[left]]--;
-                    if(m[nums[left]]==0){
-                        m.erase(nums[left]);
-                    }
-                    left++;
+    int lessthank(vector<int>&nums,int k){
+        int left=0;
+        int ans=0;
+        int l=nums.size();
+        map<int,int>m;
+        for(int i=0;i<l;i++){
+            m[nums[i]]++;
+            while(m.size()>k){
+                m[nums[left]]--;
+                if(m[nums[left]]==0){
+                    m.erase(nums[left]);
                 }
-                count+=j-left+1;
+                left++;
             }
-        return count;
+            ans=ans+i-left+1;
+        }
+        return ans;
     }
     int subarraysWithKDistinct(vector<int>& nums, int k) {
-        return countsubarray(nums,k)-countsubarray(nums,k-1);
+        int c1=lessthank(nums,k);
+        int c2=lessthank(nums,k-1);
+        return c1-c2;
     }
 };
