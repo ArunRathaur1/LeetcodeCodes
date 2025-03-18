b9a76a5cb1c7b1a2e1cb31657cb0722de81135ee
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2025-03-18 19:13:53 +0530

    Time: 3 ms (91.14%), Space: 11.1 MB (64.64%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
@@ -1,16 +1,48 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        map<char,int>m;
-        int l=s.length();
-        int ans=0;
-        int left=0;
-        for(int i=0;i<l;i++){
-            m[s[i]]++;
-            while(m.size()==3){
-                ans=ans+(l-i);
-                m[s[left]]--;
-                if(m[s[left]]==0)m.erase(s[left]);
+        // map<char,int>m;
+        // int l=s.length();
+        // int ans=0;
+        // int left=0;
+        // for(int i=0;i<l;i++){
+        //     m[s[i]]++;
+        //     while(m.size()==3){
+        //         ans=ans+(l-i);
+        //         m[s[left]]--;
+        //         if(m[s[left]]==0)m.erase(s[left]);
+        //         left++;
+        //     }
+        // }
+        // return ans;
+        int a = -1, b = -1, c = -1;
+        int counta = 0, countb = 0, countc = 0;
+        int l = s.length();
+        int ans = 0;
+        int left = 0;
+        for (int i = 0; i < l; i++) {
+            if (s[i] == 'a') {
+                counta++;
+                a = i;
+            } else if (s[i] == 'b') {
+                countb++;
+                b = i;
+            } else if (s[i] == 'c') {
+                countc++;
+                c = i;
+            }
+            while (counta != 0 && countb != 0 && countc != 0) {
+                ans = ans + l - max({a, b, c});
+                if (s[left] == 'a') {
+                    counta--;
+                    a = -1;
+                } else if (s[left] == 'b') {
+                    countb--;
+                    b = -1;
+                } else if (s[left] == 'c') {
+                    countc--;
+                    c = -1;
+                }
                 left++;
             }
         }
