09787de38308c08e4862407b8911e7d8b93e1e5a
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2025-03-18 19:01:18 +0530

    Time: 43 ms (9.96%), Space: 21 MB (5.01%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
@@ -1,33 +1,17 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        // unordered_map<char,int>m;
-        // int l=s.length();
-        // int ans=0;
-        // int left=0;
-        // int right=0;
-        // while(right<l){
-        //     m[s[right]]++;
-        //     while(m.size()==3){
-        //         ans=ans+(l-right);
-        //         m[s[left]]--;
-        //         if(m[s[left]]==0)m.erase(s[left]);
-        //         left++;
-        //         continue;
-        //     }
-        //     right++;
-        // }
-        // return ans;
-        int a=-1;
-        int b=-1;
-        int c=-1;
+        map<char,int>m;
+        int l=s.length();
         int ans=0;
-        for(int i=0;i<s.length();i++){
-            if(s[i]=='a')a=i;
-            else if(s[i]=='b')b=i;
-            else c=i;
-            if(a!=-1&& b!=-1&&c!=-1){
-                ans=ans+min({a,b,c})+1;
+        int left=0;
+        for(int i=0;i<l;i++){
+            m[s[i]]++;
+            while(m.size()==3){
+                ans=ans+(l-i);
+                m[s[left]]--;
+                if(m[s[left]]==0)m.erase(s[left]);
+                left++;
             }
         }
         return ans;
