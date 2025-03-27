5daa995f86adf3de6955e0b53d687c3334249cc6
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2025-03-27 20:10:00 +0530

    Time: 36 ms (63.87%), Space: 32.2 MB (56.77%) - LeetHub

--- 0876-hand-of-straights/0876-hand-of-straights.cpp
+++ 0876-hand-of-straights/0876-hand-of-straights.cpp
@@ -2,13 +2,12 @@ class Solution {
 public:
     bool isNStraightHand(vector<int>& hand, int s) {
         map<int,int>m;
-        int n=hand.size();
-        for(int i=0;i<n;i++){
-            m[hand[i]]++;
+        for(auto i: hand){
+            m[i]++;
         }
-        for(auto it=m.begin();it!=m.end();it++){
-            int number=it->first;
-            int count=it->second;
+        for(auto i=m.begin();i!=m.end();i++){
+            int number=i->first;
+            int count=i->second;
             if(count>0){
                 for(int i=0;i<s;i++){
                     if(m[number+i]-count<0)return false;
