b4993b57e129ef16dd28bf5e28e5706323aa8afa
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2025-03-28 10:11:40 +0530

    Time: 0 ms (100%), Space: 8.2 MB (49.66%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,36 +1,25 @@
 class Solution {
 public:
-    bool checkValid(string& s,int index,int& n,int leftcount,vector<vector<int>>&dp){
-        if(index==n){
-           if(leftcount==0)return true;
-           return false;
-        }
-        if(leftcount<0)return false;
-        if(dp[index][leftcount]!=-1)return dp[index][leftcount];
-        bool valid=false;
-
-        if(s[index]==')')
-        valid=valid||checkValid(s,index+1,n,leftcount-1,dp);
-        else if(s[index]=='(')
-        valid=valid|| checkValid(s,index+1,n,leftcount+1,dp);
-        else{
-            s[index]='A';
-            valid=valid||checkValid(s,index+1,n,leftcount,dp);
-
-            s[index]='(';
-            valid=valid||checkValid(s,index+1,n,leftcount+1,dp);
-
-            s[index]=')';
-            valid=valid||checkValid(s,index+1,n,leftcount-1,dp);
-            s[index]='*';
-        }
-        dp[index][leftcount]=valid;
-        return valid;
-    }
     bool checkValidString(string s) {
-        int p=s.length();
-        int leftcount=0;
-        vector<vector<int>>dp(p+1,vector<int>(p+1,-1));
-        return checkValid(s,0,p,leftcount,dp);
+        int mi=0;
+        int ma=0;
+        int l=s.length();
+        for(int i=0;i<l;i++){
+            if(s[i]=='('){
+                mi++;
+                ma++;
+            }
+            else if(s[i]==')'){
+                if(mi>0)mi--;
+                if(ma>0)ma--;
+                else return false;
+            }
+            else {
+                if(mi>0)mi--;
+                ma++;
+            }
+        }
+        if(mi==0)return true;
+        return false;
     }
 };
