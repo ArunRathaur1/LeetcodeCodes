5fb6ce67b3520a329aebb1badc61d031dea0c11d
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2025-03-28 08:01:11 +0530

    Time: 342 ms (18.48%), Space: 102.6 MB (12.32%) - LeetHub

--- 2588-maximum-number-of-points-from-grid-queries/2588-maximum-number-of-points-from-grid-queries.cpp
+++ 2588-maximum-number-of-points-from-grid-queries/2588-maximum-number-of-points-from-grid-queries.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     vector<int> maxPoints(vector<vector<int>>& grid, vector<int>& queries) {
-        vector<pair<int,int>>ar;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>>ar;
         int qs=queries.size();
         int row=grid.size();
         int col=grid[0].size();
-        for(int i=0;i<qs;i++)ar.push_back({queries[i],i});
-        sort(ar.begin(),ar.end());
+        for(int i=0;i<qs;i++)ar.push({queries[i],i});
         priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<>>q;
         vector<vector<bool>>visited(row,vector<bool>(col,false));
         vector<vector<int>>dir={{-1,0},{0,-1},{1,0},{0,1}};
         vector<int>ans(qs,0);
         q.push({grid[0][0],{0,0}});
-        int index=0;
         int count=0;
         visited[0][0]=true;
-        while(index<qs){
-            while(!q.empty()&& q.top().first<ar[index].first){
+        while(!ar.empty()){
+            while(!q.empty()&& q.top().first<ar.top().first){
                 pair<int,pair<int,int>>x=q.top();
                 q.pop();
                 int vlaue=x.first;
@@ -31,8 +29,8 @@ public:
                     q.push({grid[newx][newy],{newx,newy}});
                 }
             }
-            ans[ar[index].second]=count;
-            index++;
+            ans[ar.top().second]=count;
+            ar.pop();
         }
         return ans;
     }
