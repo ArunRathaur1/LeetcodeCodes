2bcaf8e68d236384798750141a1ce44f8ce74b5a
Arun Rathaur <145574851+ArunRathaur1@users.noreply.github.com>
2025-04-26 08:45:22 +0530

    Time: 0 ms (100%), Space: 93.9 MB (31.29%) - LeetHub

--- 2527-count-subarrays-with-fixed-bounds/2527-count-subarrays-with-fixed-bounds.cpp
+++ 2527-count-subarrays-with-fixed-bounds/2527-count-subarrays-with-fixed-bounds.cpp
@@ -1,28 +1,18 @@
 class Solution {
-    public:
-        long long countSubarrays(vector<int>& nums, int minK, int maxK) {
-            long long count = 0, left = 0;
-            deque<int> dq_min, dq_max;
-    
-            for (int i = 0; i < nums.size(); ++i) {
-                if (nums[i] < minK || nums[i] > maxK) {
-                    dq_min.clear();
-                    dq_max.clear();
-                    left = i + 1;
-                    continue;
-                }
-    
-                while (!dq_min.empty() && nums[dq_min.back()] >= nums[i]) dq_min.pop_back();
-                dq_min.push_back(i);
-    
-                while (!dq_max.empty() && nums[dq_max.back()] <= nums[i]) dq_max.pop_back();
-                dq_max.push_back(i);
-    
-                if (nums[dq_min.front()] == minK && nums[dq_max.front()] == maxK) {
-                    int start = min(dq_min.front(), dq_max.front());
-                    count += (start - left + 1);
-                }
-            }
-            return count;
+public:
+    long long countSubarrays(vector<int>& nums, int minK, int maxK) {
+        int m2=-1;int m1=-1;
+        int cul=-1;
+        long long ans=0;
+        int l=nums.size();
+        for(int i=0;i<l;i++){
+            if(nums[i]>maxK||nums[i]<minK)cul=i;
+            if(nums[i]==minK)m1=i;
+            if(nums[i]==maxK)m2=i;
+            int smaller=min(m1,m2);
+            long long tem=smaller-cul;
+            ans+=tem>0?tem:0;
         }
-    };
+        return ans;
+    }
+};
